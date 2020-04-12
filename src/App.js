5d7c349518440644a4e79483a71f038c7bb244c2
KeyserSoze <pavankalyan141640@gmail.com>
2020-04-12 17:34:56 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import './App.css';
+import Counter from './components/counter/Counter';
+import CounterButton from './components/counter/Counter';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+            <Counter />
+      </div>
+    );
+  }
+}
+class LearningComponents extends Component{
+  render() {
+    return (
+      <div className="LearningComponents">
+        My Hello World!
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+      </div>
+    );
+  }
+}
+export default App;
+
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import './Counter.css'
+import PropTypes from 'prop-types'
+
+
+class Counter extends Component{
+    constructor(){
+        super(); //Error 1
+        this.state={
+            counter: 0
+        }
+
+        this.increment=this.increment.bind(this);
+        this.decrement=this.decrement.bind(this);
+        this.reset=this.reset.bind(this);
+    }
+    render() {
+        return (
+          <div className="counter">
+                <CounterButton by={1} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+                <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+                <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+                <span className="count">{this.state.counter}</span>
+                <div ><button className="reset" onClick={this.reset}>Reset</button></div>
+                
+          </div>
+        );
+      }
+      increment(by) 
+      {
+        //console.log(`increment from child  - ${by}`)
+        this.setState(
+           (prevState) =>  {
+            return {counter : prevState.counter+by}
+            }
+        )
+            // this.setState({
+            //     counter: this.state.counter //+ this.props.by
+            //             });
+       }
+       decrement(by) 
+       {
+        //console.log(`increment from child  - ${by}`)
+        this.setState(
+           (prevState) =>  {
+            return {counter : prevState.counter-by}
+            }
+        )
+            // this.setState({
+            //     counter: this.state.counter //+ this.props.by
+            //             });
+       }
+       reset()
+       {
+           this.setState(
+               () => {
+                    return {counter : 0}
+               }
+           )
+       }
+}
+
+class CounterButton extends Component{
+
+
+    //Define the initial state in a constructor
+    //state => counter 0
+    constructor(){
+        super(); //Error 1
+        // this.state={
+        //     counter: 0
+        // }
+
+        // this.increment=this.increment.bind(this);
+        // this.decrement=this.decrement.bind(this);
+    }
+    render() {
+    //render = () => {
+    return (
+        <div className="Counter">
+            <button onClick={() =>this.props.incrementMethod(this.props.by)} >+{this.props.by}</button>
+            <button onClick={() =>this.props.decrementMethod(this.props.by)} >-{this.props.by}</button>
+            
+        </div>
+    )
+    }
+    // increment() {
+    // //increment = () =>{
+    //     //this.state.counter=this.state.coutner+1;
+    //     //console.log('increment');
+    //     this.setState({
+    //         counter: this.state.counter + this.props.by
+    //                 });
+    //     this.props.incrementMethod(this.props.by);
+    // }
+    // decrement(){
+    //     this.props.decrementMethod(this.props.by);
+    // }
+        
+    
+    
+}
+Counter.defaultProps={
+    by : 1
+}
+Counter.propTypes ={
+    by : PropTypes.number
+}
+export default Counter
